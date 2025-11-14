d667628990bdafdbe41b7a0a572984477ab24919
Akhil <akhilmandala001@gmail.com>
2025-11-14 10:13:16 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/cinematic-snippet-main/" : "/",   // ← FIXED
+  base: mode === "production" ? "/movie_cord/" : "/",   // ✔ FIXED
 
   server: {
     host: "::",
@@ -12,8 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
